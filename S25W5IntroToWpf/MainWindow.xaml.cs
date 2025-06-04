ba3c80ce73b8ffa08ecd0ef4a62d813bc886e536
Gursharan Singh Tatla <sing1731@shernet.sheridancollege.ca>
2025-06-04 13:40:20 -0400

    Add project files.

---
+++ S25W5IntroToWpf/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace S25W5IntroToWpf
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOk_Click(object sender, RoutedEventArgs e)
+        {
+            string name = txtName.Text;
+            MessageBox.Show("Hello " + name);
+        }
+
+        private void txtName_GotFocus(object sender, RoutedEventArgs e)
+        {
+            txtName.Background = Brushes.LightBlue;
+        }
+
+        private void txtName_LostFocus(object sender, RoutedEventArgs e)
+        {
+            txtName.Background = Brushes.White;
+        }
+    }
+}
